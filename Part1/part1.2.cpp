61d09f55050b7244c9771139df2efb1876738610
Bruno MONSUEZ <bruno.monsuez@online.fr>
2022-12-07 17:25:10 +0100

    Code cleaned.

--- Part1/part1.2.cpp
+++ Part1/part1.2.cpp
@@ -11,34 +11,32 @@ std::regex match_variables(
 
 std::map<std::string, std::string> find_all_variables(std::string filename)
 {
-    std::map<std::string, std::string> variables;
-    std::vector<double> values;
     size_t buffer_size = 80;
-    auto buffer = std::make_unique<char[]>(buffer_size);
     using iterator = char*;
+
+    auto buffer = std::make_unique<char[]>(buffer_size);
+    std::map<std::string, std::string> variables;
     std::ifstream stream(filename);
-    ptrdiff_t start = 0;
 
     while(!stream.eof() && !stream.fail())
     {
         // Load the buffer
-
         stream.getline(buffer.get(), buffer_size);
         size_t number_of_available_chars = (size_t)stream.gcount();
         while(stream.fail() && !stream.eof() 
             && number_of_available_chars == buffer_size - 1)
         {
-            // Create a larger buffer
+            // Increase the size of the buffer.
             auto new_buffer_size = buffer_size  + 40;
             auto new_buffer = std::make_unique<char[]>(new_buffer_size);
             std::copy(buffer.get(), 
                 buffer.get() + buffer_size, new_buffer.get());
 
-            // Load the upper part of the buffer/
-            stream.clear(stream.rdstate() & ~std::ios_base::failbit);
+            // Load the upper part of the buffer.
+            stream.clear();
             stream.getline(
                 new_buffer.get() + number_of_available_chars, 
-                new_buffer_size - buffer_size);
+                new_buffer_size - number_of_available_chars);
             number_of_available_chars += (size_t)stream.gcount();
 
             // Swap both buffers
@@ -46,8 +44,8 @@ std::map<std::string, std::string> find_all_variables(std::string filename)
             buffer_size = new_buffer_size;
         }
                         
-        // Look inside the buffer for all pattern that 
-        // denotes a decimal value.
+        // Test if the line that has been loaded denotes
+        // a variable definition.
         std::match_results<iterator> match;
         if(std::regex_match(buffer.get(), buffer.get() + buffer_size, 
                 match, match_variables))
